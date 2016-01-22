e3c7625bc8b7be3a939eda9533c252cc0e22019e
Alexander Ma[ẏ]orov <alexander.majorov@gmail.com>
2016-01-22 20:12:00 +0300

    Create deprecate.ts

---
+++ src/deprecate.ts
@@ -0,0 +1,24 @@
+
+
+/**
+ * 
+ * @param target
+ * @param key
+ * @param descriptor
+ * @returns {any}
+ */
+function deprecate(target:any, key:string, descriptor? :any) {
+
+	if (descriptor === undefined)
+		throw new SyntaxError('Only methods can be marked as deprecated!');
+
+	let cls = `${target.constructor.name}`;
+	let originalMethod = descriptor.value;
+
+	descriptor.value = function deprecate() {
+		console.warn(`WARNING! Method ${key}() from #${cls} is deprecated!`);
+		return originalMethod.apply(this, arguments);
+	};
+
+	return descriptor;
+}
