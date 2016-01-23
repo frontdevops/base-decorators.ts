3389c7bf27e01fb3708a180498cb33e24ce70004
Alexander Ma[ẏ]orov <alexander.majorov@gmail.com>
2016-01-23 21:01:59 +0300

    Update rtts.js

--- src/es/rtts.js
+++ src/es/rtts.js
@@ -1,20 +1,43 @@
 
-function t_number(target, key, descriptor) {
-  let newDesc = {
+function rtts(target, key, descriptor, type) {
+
+  if (type != 'function' && typeof descriptor.initializer() == 'function') {
+   throw SyntaxError(`This type annotation only for property and parameters!`);
+  }
+
+  return {
     set: function(val) {
-      let type = typeof val;
-      if (type !== 'number') {
-        throw new TypeError(`Set ${key}=${val}. Type '${type}' is not assignable to type 'number'`);
-      }
+      let ctype = typeof val;
+      if (ctype !== type)
+        throw new TypeError(`Set ${key}=${val}. Type '${ctype}' is not assignable to type '${type}'`);
       this['__meta__'+key] = val;
     },
     get: function() {
-      if (this['__meta__'+key] === undefined) {
-        this['__meta__'+key] = descriptor.initializer();
+      if (this['__meta__'+key] === undefined){
+        if (descriptor.initializer)
+          this['__meta__'+key] = descriptor.initializer();
       }
       return this['__meta__'+key];
     } 
-  }
-  
-  return newDesc;
+  }  
+}
+
+function t_number(target, key, descriptor) {
+  return rtts(target, key, descriptor, 'number');
+}
+
+function t_string(target, key, descriptor) {
+  return rtts(target, key, descriptor, 'string');
+}
+
+function t_boolean(target, key, descriptor) {
+  return rtts(target, key, descriptor, 'boolean');
+}
+
+function t_object(target, key, descriptor) {
+  return rtts(target, key, descriptor, 'object');
+}
+
+function t_function(target, key, descriptor) {
+  return rtts(target, key, descriptor, 'function');
 }
